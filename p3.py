369deee09cce83c1901c4c45d9197485387d42c2
avila <1angelovila@gmail.com>
2017-03-26 03:48:52 +0000

    nvidia model

--- p3.py
+++ p3.py
@@ -116,7 +116,7 @@ model.add(Dense(1))
 
 #########################################
 ##################LeNet##################
-
+"""
 model = Sequential()
 
 ###cropping###
@@ -134,12 +134,12 @@ model.add(Dense(120))
 model.add(Dense(84))
 model.add(Dense(1))
 
-
+"""
 
 
 #####################################
 #################Nvidia model########
-"""
+
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
@@ -153,7 +153,7 @@ model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
-"""
+
 
 
 
