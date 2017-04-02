897edb9bd4f3a488428728264313d51b4886d202
avila <1angelovila@gmail.com>
2017-04-02 11:25:19 +0000

    syntax

--- p3.py
+++ p3.py
@@ -222,7 +222,7 @@ model.add(Dense(1))
 
 model = Sequential()
 #model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160,320)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5), input_shape=(64,64,3))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(64,64,3)))
 #model.add(Convolution2D(24,5,5,subsample=(2,2), activation="relu"))
 #model.add(Convolution2D(36,5,5,subsample=(2,2), activation="relu"))
 #model.add(Convolution2D(48,5,5,subsample=(2,2), activation="relu"))
